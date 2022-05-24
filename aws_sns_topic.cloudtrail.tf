2106495de706a510c8b068af01efe9bd4f3c6ef4
Phil Dier <phil@pmg.com>
2022-05-24 11:33:21 -0500

    add sns topic policy

--- aws_sns_topic.cloudtrail.tf
+++ aws_sns_topic.cloudtrail.tf
@@ -2,3 +2,27 @@ resource "aws_sns_topic" "cloudtrail" {
   name_prefix       = "cloudtrail"
   kms_master_key_id = aws_kms_key.cloudtrail.arn
 }
+
+resource "aws_sns_topic_policy" "cloudtrail" {
+  arn    = aws_sns_topic.cloudtrail.arn
+  policy = data.aws_iam_policy_document.cloudtrail-publish.json
+}
+
+data "aws_iam_policy_document" "cloudtrail-publish" {
+  statement {
+    sid    = "AllowCloudtrailPublish"
+    effect = "Allow"
+    actions = [
+      "sns:Publish"
+    ]
+    resources = [
+      aws_sns_topic.cloudtrail.arn
+    ]
+    principals {
+      type = "AWS"
+      identifiers = [
+        "cloudtrail.amazonaws.com"
+      ]
+    }
+  }
+}
