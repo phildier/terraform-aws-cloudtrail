20021878167c3a08299caf4b95bf64d52baf4a5d
Phil Dier <phil@raventools.com>
2025-04-25 08:26:31 -0500

    readd sns policy

--- aws_sns_topic.cloudtrail.tf
+++ aws_sns_topic.cloudtrail.tf
@@ -2,3 +2,24 @@ resource "aws_sns_topic" "cloudtrail" {
   name_prefix       = "cloudtrail"
   kms_master_key_id = aws_kms_key.cloudtrail.arn
 }
+
+resource "aws_sns_topic_policy" "cloudtrail" {
+  arn = aws_sns_topic.cloudtrail.arn
+
+  policy = data.aws_iam_policy_document.cloudtrail_sns_policy.json
+}
+
+data "aws_iam_policy_document" "cloudtrail_sns_policy" {
+  statement {
+    actions = [
+      "sns:Publish"
+    ]
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_iam_role.cloudtrail.arn]
+    }
+
+    resources = [aws_sns_topic.cloudtrail.arn]
+  }
+}
